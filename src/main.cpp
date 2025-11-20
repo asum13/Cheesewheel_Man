118b5cda796f49f04ca50a9efa1ca61adc1e9b63
asum13 <asum.aliu@gmail.com>
2025-11-20 22:58:09 +0100

    Quick patch to fix coins not reseting on death

--- src/main.cpp
+++ src/main.cpp
@@ -144,6 +144,10 @@ int main()
 				player.coinsCollected = 0;
 				player.currentDirection = { 0.f, 0.f };
 				player.lookingDirection = { 0.f, 0.f };
+				for (int i = 0; i < 3; i++)
+				{
+					coinArray[i].isActive = true;
+				}
 				gameRunning = true;
 			}
 		}
